4c22ebae0a485ba83e7ab3bcb87a743b8d30c815
michelleliiiii <141749308+michelleliiiii@users.noreply.github.com>
2023-11-07 10:19:37 -0500

    Update web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -1,14 +1,1 @@
-from bs4 import BeautifulSoup
 
-'''
-with open("JNE Intranet - Home.html", "r") as f:
-    doc = BeautifulSoup(f, "html.parser")
-
-#print(doc.prettify())
-
-tag = doc.find ("a")
-print(tag)
-tag.string = "jne"
-'''
-
-import requests
